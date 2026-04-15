a215f3b1c27fc3f128c3bd66587ef08deb28e103
marvinsehunelo <marvinsehunelo@gmail.com>
2026-04-15 09:44:54 +0200

    Update sat_cashout.php

--- backend/atm/sat_cashout.php
+++ backend/atm/sat_cashout.php
@@ -6,12 +6,10 @@ $data = json_decode(file_get_contents("php://input"), true);
 $atm = new ATMService();
 
 $auth = $atm->authorizeSAT(
-    $data['sat'],
-    $data['amount']
+    (int)$data['atm_id'],
+    $data['sat_number'],
+    $data['pin'],
+    (float)$data['amount']
 );
-
-if ($auth['status'] === 'APPROVED') {
-    $atm->completeSAT($data['sat']);
-}
 
 echo json_encode($auth);
