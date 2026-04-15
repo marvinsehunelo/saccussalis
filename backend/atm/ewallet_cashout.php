21543a843e7568eeee88c4e030f4c2c064e2e63d
marvinsehunelo <marvinsehunelo@gmail.com>
2026-04-15 09:44:18 +0200

    Update ewallet_cashout.php

--- backend/atm/ewallet_cashout.php
+++ backend/atm/ewallet_cashout.php
@@ -4,10 +4,12 @@ require_once 'ATMService.php';
 $data = json_decode(file_get_contents("php://input"), true);
 
 $atm = new ATMService();
+
 echo json_encode(
     $atm->cashoutEwallet(
+        (int)$data['atm_id'],
         $data['phone'],
         $data['pin'],
-        $data['amount']
+        (float)$data['amount']
     )
 );
