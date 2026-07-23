b6c915e9752476e23059fc17eb13069dd7dc4ba0
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-23 03:02:31 +0200

    Update verify.php

--- backend/api/v1/auth/verify.php
+++ backend/api/v1/auth/verify.php
@@ -2,8 +2,8 @@
 // saccussalis/api/v1/auth/verify.php
 // POST /api/v1/auth/verify
 
-require_once __DIR__ . '/../../config/database.php';
-require_once __DIR__ . '/../../config/crypto.php';
+require_once __DIR__ . '/../../../db.php';
+require_once __DIR__ . '/../../../helpers/crypto.php';
 
 header('Content-Type: application/json');
 
